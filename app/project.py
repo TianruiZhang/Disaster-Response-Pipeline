f702dec0d8b1ab374c1eb42e3b15bd0c28768d90
Tianrui Zhang <jameschang1992@gmail.com>
2019-02-08 21:52:58 +0800

    Refact: refactor python code to comply with PEP8

--- app/project.py
+++ app/project.py
@@ -16,12 +16,12 @@ import pickle
 engine = create_engine("sqlite:///../data/messages.db")
 df = pd.read_sql("select * from messages", con=engine)
 df.rename(columns={"Search And Rescue": "Search and Rescue"}, inplace=True)
-categories = df.iloc[:,4:]
+categories = df.iloc[:, 4:]
 
 plots = []
 
 # First plot
-Categories = df.iloc[:,4:].sum().sort_values()
+Categories = df.iloc[:, 4:].sum().sort_values()
 firstData = [
     go.Bar(
         x=Categories.values,
@@ -48,15 +48,13 @@ values = list(genres.values)
 colors = ["#FEBFB3", "#E1396C", "#96D38C"]
 secondData = [
     go.Pie(
-        labels=labels, 
-        values=values, 
-        hoverinfo="label+percent", 
+        labels=labels,
+        values=values,
+        hoverinfo="label+percent",
         textinfo="value",
         marker=dict(
             colors=colors,
-            line=dict(color="#000000", 
-            width=2))
-            )
+            line=dict(color="#000000", width=2)))
 ]
 secondPlot = go.Figure(data=secondData)
 plots.append(secondPlot)
@@ -67,17 +65,17 @@ img_height = 450
 scale_factor = 1
 
 thirdLayout = go.Layout(
-    xaxis = go.layout.XAxis(
-        visible = False,
-        range = [0, img_width*scale_factor]),
-    yaxis = go.layout.YAxis(
+    xaxis=go.layout.XAxis(
         visible=False,
-        range = [0, img_height*scale_factor],
-        scaleanchor = 'x'),
-    width = img_width*scale_factor,
-    height = img_height*scale_factor,
-    margin = {'l': 0, 'r': 0, 't': 100, 'b': 0},
-    images = [go.layout.Image(
+        range=[0, img_width*scale_factor]),
+    yaxis=go.layout.YAxis(
+        visible=False,
+        range=[0, img_height*scale_factor],
+        scaleanchor="x"),
+    width=img_width*scale_factor,
+    height=img_height*scale_factor,
+    margin={'l': 0, 'r': 0, 't': 100, 'b': 0},
+    images=[go.layout.Image(
         x=0,
         sizex=img_width*scale_factor,
         y=img_height*scale_factor,
@@ -86,13 +84,14 @@ thirdLayout = go.Layout(
         yref="y",
         opacity=1.0,
         sizing="stretch",
-        source='https://c2.staticflickr.com/8/7897/46234968524_77f342bec8_o.png')]
+        source="https://c2.staticflickr.com/8/7897/"
+        "46234968524_77f342bec8_o.png")]
 )
 thirdPlot = go.Figure(data=[{
-    'x': [0, img_width*scale_factor], 
-    'y': [0, img_height*scale_factor], 
-    'mode': 'markers',
-    'marker': {'opacity': 0}}],layout = thirdLayout)
+    "x": [0, img_width * scale_factor],
+    "y": [0, img_height * scale_factor],
+    "mode": "markers",
+    "marker": {"opacity": 0}}], layout=thirdLayout)
 plots.append(thirdPlot)
 
 # Generate an ID for each plot
@@ -101,14 +100,19 @@ ids = ["plot{}".format(i) for i, _ in enumerate(plots)]
 # Convert the Plotly plots to JSON
 plotsJSON = json.dumps(plots, cls=plotly.utils.PlotlyJSONEncoder)
 
+
 # Load pre-trained model
 def tokenize(text):
     text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
     tokens = word_tokenize(text)
-    tokens = [WordNetLemmatizer().lemmatize(token) for token in tokens if token not in stopwords.words("english")]
+    tokens = [
+        WordNetLemmatizer().lemmatize(token)
+        for token in tokens
+        if token not in stopwords.words("english")]
     return tokens
 
 app = Flask(__name__)
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -118,16 +122,23 @@ def index():
             # Run prediction here
             prediction = pipeline.predict(np.array([request.form["message"]]))
             prediction = json.dumps(prediction[0].tolist())
-            print(prediction)
         except:
             print("Unable to get the message.")
     else:
         message = "No message has been received yet."
         prediction = json.dumps(np.zeros(35).tolist())
-    return render_template("index.html", dataset=categories, ids=ids, plotsJSON=plotsJSON, message=message, prediction=prediction)
+    return render_template(
+        "index.html",
+        dataset=categories,
+        ids=ids,
+        plotsJSON=plotsJSON,
+        message=message,
+        prediction=prediction
+        )
 
 if __name__ == "__main__":
     app.debug = True
-    pipeline = pickle.load(open("../models/LinearSVCClassifierChain.pkl", "rb"))
+    pipeline = pickle.load(
+        open("../models/LinearSVCClassifierChain.pkl", "rb"))
     app.run(host="0.0.0.0", port=8000)
     print("Finished Loading!")
--- data/process_data.py
+++ data/process_data.py
@@ -8,24 +8,31 @@ from nltk.corpus import stopwords
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 from PIL import Image
 
-def loadDataset(messages_path="messages.csv", categories_path="categories.csv"):
-    messages = pd.read_csv(messages_path)
-    categories = pd.read_csv(categories_path)
-    df = pd.merge(messages, categories, how="inner", on="id")
-    return df
+
+def loadDataset(messages_path="messages.csv",
+                categories_path="categories.csv"):
+                    messages = pd.read_csv(messages_path)
+                    categories = pd.read_csv(categories_path)
+                    df = pd.merge(messages, categories, how="inner", on="id")
+                    return df
+
 
 def cleanDatasets(df):
     categories = df["categories"].str.split(pat=";", expand=True)
     # Select the first row of the categories dataframe
-    row = categories.iloc[0,:]
-    category_colnames = [i[:i.find("-")].title().replace("_", " ") for i in list(row)]
+    row = categories.iloc[0, :]
+    category_colnames = [
+        i[:i.find("-")].title().replace("_", " ")
+        for i in list(row)]
     # Rename the columns of `categories`
     categories.columns = category_colnames
     for column in categories:
         # Set each value to be the last character of the string
         categories[column] = categories[column].str[-1]
         # Convert each column from string to integer
-        categories[column] = pd.to_numeric(categories[column], downcast="integer")
+        categories[column] = pd.to_numeric(
+            categories[column],
+            downcast="integer")
     # Link each ID to its categories
     categories = pd.concat([df["id"], categories], axis=1)
     # Drop rows where Related equals 2
@@ -37,9 +44,9 @@ def cleanDatasets(df):
     # Rename columns
     df.rename(columns={
         "id": "ID",
-        "message": "Message", 
-        "original": "Original", 
-        "genre": "Genre"}, 
+        "message": "Message",
+        "original": "Original",
+        "genre": "Genre"},
         inplace=True)
     # Merge the original dataframe with the new `categories` dataframe
     df = df.merge(categories, how="inner", left_on="ID", right_on="id")
@@ -48,9 +55,11 @@ def cleanDatasets(df):
     df.drop_duplicates(inplace=True)
     return df
 
+
 def save2DB(df):
     engine = create_engine("sqlite:///messages.db")
     df.to_sql("messages", engine, index=False, if_exists="replace")
+
 
 def generateWordCloud(df):
     words = " ".join(message for message in df.Message)
@@ -60,7 +69,9 @@ def generateWordCloud(df):
     stopwords = set(STOPWORDS)
     words = [word for word in words if word not in stopwords]
     words = " ".join(words)
-    wordcloud = WordCloud(stopwords=stopwords, background_color="white").generate(words)
+    wordcloud = WordCloud(
+        stopwords=stopwords,
+        background_color="white").generate(words)
     wordcloud.to_file("wordcloud.png")
 
 if __name__ == "__main__":
